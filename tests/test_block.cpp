d152743e9cde811ed19365a0cc4e4f2c9004de9a
SchrodingerZhu <i@zhuyi.fan>
2020-02-27 18:20:51 +0800

    fix test

--- tests/test_block.cpp
+++ tests/test_block.cpp
@@ -29,128 +29,81 @@ void test_block(const std::string &content, std::vector<uint32_t> expected) {
 
 int main() {
     test_mode(true);
-    test_block("L: add $t0, $t1, $t2\n"
-               "M: j L", {0x012a4020, 0x08100000});
-    test_block("L: add $t0, $t1, $t2\n"
-               "M: bne $v0, $v1, L\n"
-               "add $t8, $t9, $ra \n"
-               "jal M", {0x012a4020, 0x1443fffe, 0x033fc020, 0x0c100001});
-    test_block("addi    $sp,   $sp, -8 \n"
-               "sw      $ra, 0($sp) \n"
-               "j LABEL_DONE\n"
-               "LABEL_DONE: \n"
+    test_block("R:\tadd $s0, $s1, $s2\t#r instructions\n"
+               "\taddu $s0, $s1, $s2\n"
+               "\tsub $s0, $s1, $s2\n"
+               "\tsubu $s0, $s1, $s2\n"
+               "\tand $s0, $s1, $s2\n"
+               "\tor $s0, $s1, $s2\n"
+               "\tnor $s0, $s1, $s2\n"
+               "        xor $t0, $t1, $t2\n"
+               "\tdiv $t1, $t2\n"
+               "\tdivu $t4, $t2\n"
+               "\tsll $s0, $s1, 10\n"
+               "\tsrl $s0, $s1, 10\n"
+               "\tslt $s0, $s1, $s3\n"
+               "\tsltu $s0, $s1, $s3\n"
+               "\tjr $ra\n"
+               "\tmfhi $t3\n"
+               "\tmflo $s2\n"
+               "\tmult $t2, $t3\n"
+               "\tmultu $t1, $t0\n"
+               "\t\n"
                "\n"
-               "jr $ra", {0x23bdfff8, 0xafbf0000, 0x08100003, 0x03e00008});
-    std::vector<uint32_t> list = {
-            0x23bdfff8,
-            0xafb00004,
-            0xafbf0000,
-            0x14800002,
-            0x20a20001,
-            0x08100012,
-            0x14a00004,
-            0x2084ffff,
-            0x20050001,
-            0x0c100000,
-            0x08100012,
-            0x00808020,
-            0x20a5ffff,
-            0x0c100000,
-            0x2204ffff,
-            0x00402820,
-            0x0c100000,
-            0x08100012,
-            0x8fb00004,
-            0x8fbf0000,
-            0x23bd0008,
-            0x03e00008,
-    };
-    auto text = "########################################################\n"
-                "# Ackermann's function implementation using MIPS assembly\n"
-                "# C interface:\n"
-                "#            int AckermannFunc (int m,int n)\n"
-                "########################################################\n"
-                "\n"
-                "\n"
-                "\n"
-                "# Preconditions:   \n"
-                "#   1st parameter ($a0) m\n"
-                "#   2nd parameter ($a1) n\n"
-                "# Postconditions:\n"
-                "#   result in ($v0) = value of A(m,n)\n"
-                "\n"
-                "# we are going to use $s0 as a temporary registers to store m sometimes\n"
-                "\n"
-                "AckermannFunc:   \n"
-                "             # make space on stack\n"
-                "            addi    $sp,   $sp, -8          \n"
-                "            # preserve registers used by this function \n"
-                "              sw      $s0, 4($sp)\n"
-                "            # preserve return address            \n"
-                "              sw      $ra, 0($sp)     \n"
-                "\n"
-                "            # move the parameter registers to temporary  - no, only when nec.\n"
-                "\n"
-                "LABEL_IF:      # check whether m==0\n"
-                "            # if not then branch to LABEL_ELSE_IF\n"
-                "            bne   $a0, $zero, LABEL_ELSE_IF\n"
-                "\n"
-                "            #  code for \"result = n+1\"\n"
-                "            addi   $v0,   $a1,   1\n"
-                "\n"
-                "            # jump to LABEL_DONE\n"
-                "            j LABEL_DONE\n"
-                "\n"
-                "\n"
-                "LABEL_ELSE_IF:\n"
-                "            # check whether n==0\n"
-                "            #if not then branch to LABEL_ELSE\n"
-                "            bne $a1, $zero,   LABEL_ELSE\n"
-                "\n"
-                "            # need to call A(m-1,1)\n"
-                "            # so initiate $a0,$a1 with m-1 and 1 \n"
-                "            addi   $a0,   $a0,   -1\n"
-                "            addi   $a1,   $zero,   1\n"
-                "\n"
-                "\n"
-                "            # call AckermannFunc\n"
-                "            jal   AckermannFunc\n"
-                "            # Return value already in $v0         \n"
-                "\n"
-                "            # jump to LABEL_DONE\n"
-                "            j LABEL_DONE\n"
-                "\n"
-                "LABEL_ELSE:      # This block may be a bit tricky !\n"
-                "            ###################################\n"
-                "\n"
-                "            # Save \"m\" to preserve it for the second ackermann call\n"
-                "              add     $s0, $a0, $zero\n"
-                "\n"
-                "            # call to acker(m, (n - 1))\n"
-                "              addi    $a1, $a1, -1\n"
-                "              jal AckermannFunc\n"
-                "            # return value will be used very soon!\n"
-                "\n"
-                "            # call to acker(m-1, acker(m, (n - 1)))\n"
-                "            # Take the \"m\" we saved and decrement it to be the new \"m-1\" :)\n"
-                "           addi    $a0, $s0, -1\n"
-                "           add     $a1, $v0, $zero\n"
-                "           jal AckermannFunc\n"
-                "\n"
-                "            # jump to LABEL_DONE\n"
-                "              j   LABEL_DONE\n"
-                "\n"
-                "LABEL_DONE:            \n"
-                "            # ALREADY loaded the return value register $v0 with result.\n"
-                "\n"
-                "            # restore registers used by this function\n"
-                "            lw   $s0,   4($sp)\n"
-                "            # restore return address \n"
-                "            lw   $ra,   0($sp)\n"
-                "            # restore stack pointer\n"
-                "            addi   $sp,   $sp,   8\n"
-                "\n"
-                "            # return from this function\n"
-                "            jr $ra";
-    test_block(text, list);
+               "\n"
+               "I:\taddi $s0, $s1, -100\t#i instructions\n"
+               "\taddiu $s0, $s1, 100\n"
+               "\tandi $s0, $s1, 100\n"
+               "\tori $s0, $s1, 100\n"
+               "\tslti $s0, $s1, 100\n"
+               "\tsltiu $s0, $s1, 100\n"
+               "\tbeq $s0, $s1, I\n"
+               "\tbne $s0, $s1, R\n"
+               "\tbgez $s0, I\n"
+               "\tbltz $s1, I\n"
+               "\tlw $s0, 100($s1)\n"
+               "\tsw $s0, 100($s1)\n"
+               "\tlui $s0, 100\n"
+               "\n"
+               "\n"
+               "\n"
+               "\n"
+               "J:\tj I\t\t\t#j instructions\n"
+               "\tjal R", {
+            0b00000010001100101000000000100000,
+            0b00000010001100101000000000100001,
+            0b00000010001100101000000000100010,
+            0b00000010001100101000000000100011,
+            0b00000010001100101000000000100100,
+            0b00000010001100101000000000100101,
+            0b00000010001100101000000000100111,
+            0b00000001001010100100000000100110,
+            0b00000001001010100000000000011010,
+            0b00000001100010100000000000011011,
+            0b00000000000100011000001010000000,
+            0b00000000000100011000001010000010,
+            0b00000010001100111000000000101010,
+            0b00000010001100111000000000101011,
+            0b00000011111000000000000000001000,
+            0b00000000000000000101100000010000,
+            0b00000000000000001001000000010010,
+            0b00000001010010110000000000011000,
+            0b00000001001010000000000000011001,
+            0b00100010001100001111111110011100,
+            0b00100110001100000000000001100100,
+            0b00110010001100000000000001100100,
+            0b00110110001100000000000001100100,
+            0b00101010001100000000000001100100,
+            0b00101110001100000000000001100100,
+            0b00010010000100011111111111111001,
+            0b00010110000100011111111111100101,
+            0b00000110000000011111111111110111,
+            0b00000110001000001111111111110110,
+            0b10001110001100000000000001100100,
+            0b10101110001100000000000001100100,
+            0b00111100000100000000000001100100,
+            0b00001000000000000000000000010011,
+            0b00001100000000000000000000000000,
+    });
+    
 }
